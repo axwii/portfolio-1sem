5dd4331ebac19db7dc7032c70fb12acce7904d23
axwii <axwi0001@stud.kea.dk>
2024-01-10 00:23:07 +0100

    updates to js, seperate cards js

--- js/script.js
+++ js/script.js
@@ -14,15 +14,3 @@ links.forEach((link) => {
     nav.classList.remove("active");
   });
 });
-
-// Cards
-document.getElementById("cards").onmousemove = e => {
-  for(const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
-
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
-  };
-}
